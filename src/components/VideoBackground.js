17966fa65f18bf0d1de217fcf899ea763b0c333f
Sagar SOnkar <sagarsonker56625@gmail.com>
2025-09-18 00:03:35 +0530

    update

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -5,10 +5,11 @@ export const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
   const trailerVideo = useSelector((state) => state.movie.trailerVideo);
   if (!trailerVideo) return null;
+  const videoId = trailerVideo.key;
   return (
     <div className="aspect-video">
-      <iframe className="aspect-video"
-        src={"https://www.youtube.com/embed/" + trailerVideo.key + "?autoplay=1&mute=1"}
+      <iframe className="aspect-video w-full"
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
